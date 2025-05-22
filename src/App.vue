64aee1ee73a6942abb2d024876a5614afa382c6b
Othman Haba  (ง'̀-'́)ง <124089971+OthmanHaba@users.noreply.github.com>
2025-05-22 22:31:18 +0200

    setup react query

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 const locale = localStorage.getItem('locale') || 'ar';
-
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
 </script>
 
 <template>
     <div :dir="locale == 'ar' ? 'rtl' : 'ltr'">
         <RouterView />
     </div>
+    <VueQueryDevtools />
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 import { createI18n } from 'vue-i18n'
 import en from './lang/en'
 import ar from './lang/ar'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const locale = localStorage.getItem('locale') || 'ar'
 localStorage.setItem('locale', locale)
@@ -24,5 +24,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+app.use(VueQueryPlugin)
 
 app.mount('#app')
